9c5aeef50b5889dc68254b3d503f700577e16d6d
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2018-02-09 19:28:37 +0100

    Added confirmation for clean command.

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,17 +1,35 @@
 const shell = require('shelljs');
+const readline = require('readline');
 const {exec} = require('child_process');
 
-if (!shell.which('git')) {
-    shell.echo('Sorry, this script requires git');
-    shell.exit(1);
-}
+const rl = readline.createInterface({input: process.stdin, output: process.stdout});
 
-shell.rm('-rf', '.git');
-
-exec('git init && git add . && git commit -m "Initial Commit."', (err, stdout, stderr) => {
-    if (err) {
-        console.error(err);
+rl.question('Do you really want to clean this Repository and initialize a new one? [y/n]', (answer) => {
+    if (answer.toLowerCase() === 'y' || answer.toLowerCase() === 'yes') {
+        console.log(`Answer [${answer}] received. Starting clean process...`);
+        rl.close();
+        clean();
+    } else {
+        console.log(`Answer [${answer}] received. Stopping process.`);
+        rl.close();
         return;
     }
-    console.log(stdout);
 });
+
+function clean() {
+    console.log("still here");
+    if (!shell.which('git')) {
+        shell.echo('Sorry, this script requires git');
+        shell.exit(1);
+    }
+
+    shell.rm('-rf', '.git');
+
+    exec('git init && git add . && git commit -m "Initial Commit."', (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(stdout);
+    });
+}
